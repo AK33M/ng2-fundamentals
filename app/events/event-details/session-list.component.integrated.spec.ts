7caaf77d566406871ace66f643d0f92fdc850fc4
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-04-06 17:03:09 +0100

    Deep integrated tests

--- app/events/event-details/session-list.component.integrated.spec.ts
+++ app/events/event-details/session-list.component.integrated.spec.ts
@@ -2,6 +2,9 @@ import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {DebugElement} from '@angular/core'
 import {SessionListComponent} from './session-list.component'
 import {AuthService} from '../../user/auth.service'
+import {DurationPipe} from '../shared/duration.pipe'
+import {CollapsibleWellComponent} from '../../common/collapsible-well.component'
+import {UpvoteComponent} from './upvote.component'
 import {VoterService} from './voter.service'
 import {ISession} from '../shared/event.model'
 import {By} from '@angular/platform-browser'
@@ -14,13 +17,23 @@ describe('SessionListComponent', () => {
         debugEl: DebugElement
 
     beforeEach(async(() => {
-        let mockAuthService = {};
-        let mockVoterService = {};
+        let mockAuthService = {
+            isAuthenticated: () => true,
+            currentUser: {
+              userName: 'Joe'
+            }
+        };
+        let mockVoterService = {
+          userHasVoted: () => true
+        };
 
         TestBed.configureTestingModule({
             imports: [],
             declarations: [
-                SessionListComponent
+                SessionListComponent,
+                UpvoteComponent,
+                DurationPipe,
+                CollapsibleWellComponent
             ],
             providers: [
                 { provide: AuthService, useValue: mockAuthService },
@@ -30,10 +43,28 @@ describe('SessionListComponent', () => {
         }).compileComponents();
     }))
 
-    beforeEach (()=> {
-      fixture = TestBed.createComponent(SessionListComponent);
-      component = fixture.componentInstance;
-      element = fixture.nativeElement;
-      debugEl = fixture.debugElement;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SessionListComponent);
+        component = fixture.componentInstance;
+        element = fixture.nativeElement;
+        debugEl = fixture.debugElement;
+    })
+
+    describe('Initial display', () => {
+        it('should have the correct session title', () => {
+            component.sessions = [{
+                id: 3, name: 'Session 1',
+                presenter: 'Joe', duration: 1, level: 'beginner',
+                abstract: 'abstract', voters: ['john', 'bob']
+            }];
+            component.filterBy = 'all';
+            component.sortBy = 'name';
+            component.eventId = 4;
+
+            component.ngOnChanges();
+            fixture.detectChanges();
+
+            expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+        })
     })
 })
